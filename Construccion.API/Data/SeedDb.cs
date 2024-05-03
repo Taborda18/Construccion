f51b7e208b8215e4276d75ec57917cfedfcbf1d6
ROBINSON DIONISIO HENAO NORIEGA <robinsonhenao328617@correo.itm.edu.co>
2024-05-03 18:37:18 -0500

    Intentando meter user

---
+++ Construccion.API/Data/SeedDb.cs
@@ -0,0 +1,72 @@
+﻿using Construccion.API.Helpers;
+using Construccion.Shared.Entities;
+using Construccion.Shared.Enums;
+using System.Diagnostics.Eventing.Reader;
+using System.Diagnostics.Metrics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Azure.Identity;
+using Azure;
+namespace Construccion.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+
+        public SeedDb(DataContext context, IUserHelper userHelper)
+        {
+            _context = context;
+            _userHelper = userHelper;
+        }
+
+
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            
+            //await CheckRolesAsync();
+            //await CheckUserAsync("1010", "OAP", "Admin", "orlapez@gmail.com", "305232456", "Cr 45 7896", UserType.Admin);
+
+
+
+
+        }
+
+        private async Task CheckRolesAsync()
+        {
+            await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
+            await _userHelper.CheckRoleAsync(UserType.User.ToString());
+        }
+
+
+
+        private async Task<User> CheckUserAsync(string document, string firstName, string lastName, string email, string phone, string address, UserType userType)
+        {
+            var user = await _userHelper.GetUserAsync(email);
+
+            if (user == null)
+            {
+
+                user = new User
+                {
+                    Document = document,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    PhoneNumber = phone,
+                    UserName = email,
+                    Address = address,
+                    UserType = userType,
+                };
+
+                await _userHelper.AdduserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
+            }
+            return user;
+        }
+
+        
+    }
+}
---
+++ Construccion.API/Helpers/IUserHelper.cs
@@ -0,0 +1,19 @@
+﻿using Construccion.Shared.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Construccion.API.Helpers
+{
+    public interface IUserHelper
+
+    {
+
+        Task<User> GetUserAsync(string email);
+        Task<IdentityResult> AdduserAsync(User user, string password);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+    }
+}
